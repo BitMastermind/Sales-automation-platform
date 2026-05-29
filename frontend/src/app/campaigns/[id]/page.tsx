2447899e0d1461d7565b5217152ad3eab79448c1
BitMastermind <ashitverma.211it012@nitk.edu.in>
2026-05-29 09:21:41 +0530

    made frontend changes

--- frontend/src/app/campaigns/[id]/page.tsx
+++ frontend/src/app/campaigns/[id]/page.tsx
@@ -1,6 +1,7 @@
+import { use } from "react"
 import CampaignDetailClient from "@/app/campaigns/[id]/CampaignDetailClient"
 
-export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params)
   return <CampaignDetailClient campaignId={id} />
 }
